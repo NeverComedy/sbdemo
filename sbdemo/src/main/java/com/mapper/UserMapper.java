4757a58d543551f782af1c16366ddb9f228c177d
ZhangHui <1120687935@qq.com>
2019-12-19 15:46:04 +0800

    第三次修改提交

---
+++ sbdemo/src/main/java/com/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.mapper;
+
+import com.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+@Mapper
+@Component
+public interface UserMapper {
+
+    //用户登录
+    User userLogin(@Param("username") String username, @Param("password") String password);
+
+    //用户注册
+    int register(User user);
+
+    //用户基本信息修改
+    int userUpdateInfo(@Param("sex")String sex,@Param("age")int age,@Param("username")String username);
+
+    //用户信息查询
+    User userInfo(@Param("username")String username);
+}
